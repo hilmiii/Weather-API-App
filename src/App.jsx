0097a24888c46d73608ae5743883e80fc1776759
hilmiii <hilmiawananggoro189@gmail.com>
2025-12-17 16:36:51 +0700

    alarm1

--- src/App.jsx
+++ src/App.jsx
@@ -40,15 +40,15 @@ const App = () => {
 
 const playAlarm = () => {
   setIsAlarmPlaying(true);
-  
-  // 1. Bunyi di Browser
   audioRef.current.play().catch(error => console.log(error));
 
-  // 2. Kirim sinyal ke ESP8266 (Ganti dengan IP ESP8266 Anda)
-  const ESP_IP = "192.168.4.1"; 
-  fetch(`http://${ESP_IP}/trigger-alarm`)
-    .then(() => console.log("ESP8266 Alarm Triggered"))
-    .catch(err => console.error("Gagal menghubungi ESP8266", err));
+  const ESP_IP = "192.168.1.8"; // Pastikan IP ini benar (bukan 192.168.4.1 jika sudah terhubung WiFi)
+  
+  fetch(`http://${ESP_IP}/trigger-alarm`, {
+    mode: 'no-cors', // Menghindari beberapa batasan browser
+  })
+  .then(() => console.log("Sinyal terkirim ke ESP8266"))
+  .catch(err => console.error("Gagal menghubungi ESP8266", err));
 };
 
   const stopAlarm = () => {
